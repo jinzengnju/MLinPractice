6e2ed41f31e42de8bbc4e335cc1586a6910ceb99
曾进 <zengjin@xiaomi.com>
2020-11-23 19:57:30 +0800

    update

---
+++ TransferLearning/transfer.py
@@ -0,0 +1,23 @@
+# -*- coding:UTF-8 -*-
+
+import tensorflow as tf
+base_model = keras.applications.Xception(
+    weights='imagenet',  # Load weights pre-trained on ImageNet.
+    input_shape=(150, 150, 3),
+    include_top=False)  # Do not include the ImageNet classifier at the top.
+
+base_model.trainable = False
+inputs = keras.Input(shape=(150, 150, 3))
+# We make sure that the base_model is running in inference mode here,
+# by passing `training=False`. This is important for fine-tuning, as you will
+# learn in a few paragraphs.
+x = base_model(inputs, training=False)
+# Convert features of shape `base_model.output_shape[1:]` to vectors
+x = keras.layers.GlobalAveragePooling2D()(x)
+# A Dense classifier with a single unit (binary classification)
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
+model.compile(optimizer=keras.optimizers.Adam(),
+              loss=keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=[keras.metrics.BinaryAccuracy()])
+model.fit(new_dataset, epochs=20, callbacks=..., validation_data=...)
